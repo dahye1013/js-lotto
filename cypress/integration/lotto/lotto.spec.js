b88e454df22d69a2d3ac7d271641888e64538203
dahye1013 <alrean@naver.com>
2022-03-25 15:09:46 +0900

    test: 당첨번호 테스트 목업데이터 수정

--- cypress/integration/lotto/lotto.spec.js
+++ cypress/integration/lotto/lotto.spec.js
@@ -128,7 +128,7 @@ describe('로또 미션 Cypress', () => {
       typedPrice: 5000,
       duplicatedNumbers: [[1, 3, 12, 5, 4, 2], 10],
       overRangeNumbers: [[47, 1, 12, 30, 23, 2], 10],
-      emptyRangeNumbers: [[1, 12, 30, 23, 2, 10], 14],
+      emptyRangeNumbers: [[1, 12, 30, 23, 2, ' '], 14],
       validWinningNumbers: [[1, 3, 12, 5, 4, 2], 23],
     };
 
@@ -136,8 +136,8 @@ describe('로또 미션 Cypress', () => {
       cy.purchaseLotto(mockData.typedPrice);
     });
 
-    context('1. 결과를 확인 할 수 없는 입력', () => {
-      it.only('(1) 중복 로또 번호 입력 테스트', () => {
+    context.only('1. 결과를 확인 할 수 없는 입력', () => {
+      it('(1) 중복 로또 번호 입력 테스트', () => {
         cy.typeWinningNumbers(mockData.duplicatedNumbers);
         cy.get('.open-result-modal-button').click();
         cy.testProperMessage('로또 번호에는 중복된 숫자를 입력할 수 없습니다.');
@@ -145,13 +145,13 @@ describe('로또 미션 Cypress', () => {
       });
 
       it('(2) 로또 범위 1 ~ 45 범위를 벗어난 숫자 입력 테스트', () => {
-        cy.typeWinningNumbers(mockData.duplicatedNumbers);
+        cy.typeWinningNumbers(mockData.overRangeNumbers);
         cy.get('.open-result-modal-button').click();
         cy.get('.modal').should('not.be.visible');
       });
 
       it('(3) 빈 값이 존재하는 입력 테스트', () => {
-        cy.typeWinningNumbers(mockData.duplicatedNumbers);
+        cy.typeWinningNumbers(mockData.emptyRangeNumbers);
         cy.get('.open-result-modal-button').click();
         cy.get('.modal').should('not.be.visible');
       });
